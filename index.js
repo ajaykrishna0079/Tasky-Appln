fe180338554257d17d2cb6ca8da58b42d442c35e
ajaykrishna0079 <120189249+ajaykrishna0079@users.noreply.github.com>
2023-04-22 10:38:05 +0000

    done

--- index.js
+++ index.js
@@ -37,7 +37,7 @@
 const state = {
     taskList: [],
   };
-  
+
   // DOM Operations
   const taskContents = document.querySelector(".task__contents");
   const taskModal = document.querySelector(".task__modal__body");
